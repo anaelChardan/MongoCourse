2f081168b0cfb1603ab5908677caf963c29efcd0
anaelChardan <anael.chardan@gmail.com>
2016-11-22 00:49:29 +0100

    UPDATE

--- scripts/setupReplicatSet.sh
+++ scripts/setupReplicatSet.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
+echo "5";
+sleep 1;
+echo "4";
+sleep 1;
+echo "3";
+sleep 1;
+echo "2";
+sleep 1;
+echo "1";
+sleep 1;
+echo "0";
+sleep 1;
+
 mongo --host mongodb_secondary_1 < /scripts/init_replica.js
+
+mongod
